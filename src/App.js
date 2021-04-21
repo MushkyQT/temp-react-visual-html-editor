d72bb2ae0e86412bac199950fe3e9182ce8c08f1
Charles <charlesmelki@gmail.com>
2021-04-21 15:04:43 +0200

    Moved toolbox config array up to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,82 @@
 import BlocksToolbox from "./features/blocks/BlocksToolbox";
+
 // import {useSelector} from "react-redux"
 
 function App() {
   //const isLogged = useSelector(state => state.logged)
+  const categoryStyles = [
+    {
+      id: 1,
+      name: 'Blocks',
+      highlight: '#4a90e2',
+      blocks: [
+        {
+          type: '1'
+        },
+        {
+          type: '2'
+        },
+        {
+          type: '3'
+        },
+        {
+          type: '16'
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Forms',
+      highlight: '#b8e986',
+      blocks: [
+        {
+          type: '4'
+        },
+        {
+          type: '5'
+        },
+        {
+          type: '6'
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Inline',
+      highlight: '#4a51e2',
+      blocks: [
+        {
+          type: '7'
+        },
+        {
+          type: '8'
+        },
+        {
+          type: '9'
+        }
+      ]
+    },
+    {
+      id: 4,
+      name: 'Attributes',
+      highlight: '#c346f0',
+      blocks: [
+        {
+          type: '10'
+        },
+        {
+          type: '11'
+        },
+        {
+          type: '12'
+        }
+      ]
+    },
+  ]
 
   return (
     <div className="App">
-      <BlocksToolbox/>
+      <BlocksToolbox categories={categoryStyles}/>
       {/*{isLogged ? <h3>Hello boyo</h3> : ''}*/}
     </div>
   )
--- src/features/blocks/BlocksToolbox.js
+++ src/features/blocks/BlocksToolbox.js
@@ -1,80 +1,10 @@
 import BlocksCategory from "./BlocksCategory"
 import './BlocksToolbox.css'
 
-function BlocksToolbox() {
-  const categoryStyles = [
-    {
-      id: 1,
-      name: 'Blocks',
-      highlight: '#4a90e2',
-      blocks: [
-        {
-          type: '1'
-        },
-        {
-          type: '2'
-        },
-        {
-          type: '3'
-        },
-        {
-          type: '16'
-        }
-      ]
-    },
-    {
-      id: 2,
-      name: 'Forms',
-      highlight: '#b8e986',
-      blocks: [
-        {
-          type: '4'
-        },
-        {
-          type: '5'
-        },
-        {
-          type: '6'
-        }
-      ]
-    },
-    {
-      id: 3,
-      name: 'Inline',
-      highlight: '#4a51e2',
-      blocks: [
-        {
-          type: '7'
-        },
-        {
-          type: '8'
-        },
-        {
-          type: '9'
-        }
-      ]
-    },
-    {
-      id: 4,
-      name: 'Attributes',
-      highlight: '#c346f0',
-      blocks: [
-        {
-          type: '10'
-        },
-        {
-          type: '11'
-        },
-        {
-          type: '12'
-        }
-      ]
-    },
-  ]
-
+function BlocksToolbox({categories}) {
   return (
     <div className={'blocks-toolbox'}>
-      {categoryStyles.map(category => {
+      {categories.map(category => {
         return <BlocksCategory
           key={category.id}
           name={category.name}
